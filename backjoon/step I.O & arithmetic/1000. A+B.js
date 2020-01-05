04f25f48c8021fe75c1ea0009d3feb83d79d98bd
Isa(yunjongryu) <yunjonglyu@gmail.com>
2020-01-05 23:31:29 +0900

    UPDATE step 1 example line fix - step1

--- backjoon/step I.O & arithmetic/1000. A+B.js
+++ backjoon/step I.O & arithmetic/1000. A+B.js
@@ -1,5 +1,5 @@
 (function main(a, b){
-console.log(a + b);
+    console.log(a + b);
 })();
 
 /* 백준은 값을 인자로 주는게 아니라 읽어와야한다
--- backjoon/step I.O & arithmetic/1008. A나누기B.js
+++ backjoon/step I.O & arithmetic/1008. A나누기B.js
@@ -1,9 +1,9 @@
 (function main(a, b){
     console.log(a / b);
-    })();
+})();
     
-    /* 백준은 값을 인자로 주는게 아니라 읽어와야한다
-        var fs = require('fs');
-        var input = fs.readFileSync('/dev/stdin').toString().split(' ');
-        console.log(parseInt(input[0])/parseInt(input[1]));
-    */
+/* 백준은 값을 인자로 주는게 아니라 읽어와야한다
+    var fs = require('fs');
+    var input = fs.readFileSync('/dev/stdin').toString().split(' ');
+    console.log(parseInt(input[0])/parseInt(input[1]));
+*/
