2427ffd6a71b9d04b4039ae056f8ccbaf38dbaf0
Isa(yunjongryu) <yunjonglyu@gmail.com>
2020-11-05 01:00:22 +0900

    fix: LEECODE EXAMPLE 6 - 1~100/ZIGZAG CONVERSION

--- leecode/1~100/6.ZigZag Conversion.js
+++ leecode/1~100/6.ZigZag Conversion.js
@@ -3,7 +3,7 @@
  * @param {number} numRows
  * @return {string}
  */
-const convert = function(s, numRows) {
+const convert = function(s, numRows) { // 너무 더러워서 손절 ㅜㅜ
     let result = '';
     const state = [];
     for(let int = 0; int < numRows; int++){
@@ -11,18 +11,22 @@ const convert = function(s, numRows) {
     }
     const zigCount = parseInt(numRows / 2);
     const index = [0, 1, 0];
-
     for(let int = 0; int < s.length; int++){
-        if(index[1] % 2 === 0){
-            while(index[0] < ((numRows - zigCount - 1) + index[2])){
+        if(index[1] % 2 === 0 && numRows !== 2){
+            while(index[0] < (zigCount - index[2])){
                 state[index[0]].push('');
                 index[0]++;
+            }
+
+            index[2]++;
+            if(index[2] >= zigCount){
+                index[2] = 0;
             }
         }
 
         state[index[0]].push(s[int]);
         
-        if(index[1] % 2 === 0){
+        if(index[1] % 2 === 0 && numRows !== 2){
             while((index[0] + 1) < numRows){
                 index[0]++;
                 state[index[0]].push('');
@@ -43,13 +47,55 @@ const convert = function(s, numRows) {
 
     return result;
 };
+/**
+ *  다시 생각한 코드
+ */
+const convert = function(s, numRows) {
+    let result = '';
+    const state = [];
+    const index = [0, true];
+
+    for(let int = 0; int < numRows; int++){
+        state.push([]);
+    }
+    if(numRows > 1){
+        for(let int = 0; int < s.length; int++){
+            state[index[0]].push(s[int]);
+            
+            if((index[0] + 1) <= numRows && index[1] === true){ // zig
+                index[0]++;
+            } else if((index[0] - 1) >= -1 && index[1] === false){
+                index[0]--;
+            }
+            
+            if(index[0] === numRows){
+                index[0] -= 2;
+                index[1] = false;
+            } else if (index[0] === -1){
+                index[0] += 2;
+                index[1] = true;
+            }
+        }
+    
+        result = state.map((arr) => arr.join('')).join('');
+    } else {
+        result = s;
+    }
+
+
+    return result;
+};
 
 /**
- *  1차 테스트케이스 통과 일단 지그재그를 만들고, 그다음에 사이사이 규칙을 찾아서 적용시키는 과정에서 막혔다.
- *  오늘은 컨디션이 안좋으니 다음에 다시 생각하자.
+ * Runtime: 112 ms, faster than 58.60% of JavaScript online submissions for ZigZag Conversion.
+Memory Usage: 45.7 MB, less than 5.34% of JavaScript online submissions for ZigZag Conversion.
  */
 
+convert("AB", 1);
+convert("ABC", 2);
+convert("ABCD", 2);
 convert("PAYPALISHIRING", 4);
 
 convert("PAYPALISHIRING", 3);
 convert("A", 1);
+
