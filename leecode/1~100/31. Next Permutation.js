eeb9daa90266d32546dcdea3606e43fb049b7222
Isa(yunjongryu) <yunjonglyu@gmail.com>
2021-02-12 17:51:43 +0900

    fix: LEETCODE EXAMPLE 31 - 1~100/NEXT PERMUTATION

--- leecode/1~100/31. Next Permutation.js
+++ leecode/1~100/31. Next Permutation.js
@@ -5,32 +5,35 @@
 const nextPermutation = function(nums) {
     const result = nums;
 
-    let int = result.length - 1;
-    for(; int > 0; int--){
-        if(result[int] >= result[int - 1]){
+    let int = (result.length - 2);
+    for(; int >= 0; int--){
+        if(result[int] < result[int + 1]){
             break;
         }         
     }
-    if(int === 0){
+
+    if(int === -1){
         result.reverse();
     } else {
-        for(let idx = int; idx >= 0; idx--){
-            if(result[idx] < result[int]){
-                const tempNums = result[int];
-                result[int] = result[idx];
-                result[idx] = tempNums;
-                if(idx === 0 && result.length > 2){
-                    result.reverse();
-                }
-                break;
-            }
-        }
+        let idx = (result.length - 1);
+        for(; result[idx] <= result[int]; idx--);
+        let tempNum = result[int];
+
+        result[int] = result[idx];
+        result[idx] = tempNum;
+
+        for(int++, idx = (result.length - 1); int < idx; int++, idx--){
+            tempNum = result[int];
+            result[int] = result[idx];
+            result[idx] = tempNum;
+        }    
     }
 
     return result;
 };
-/** 순열 자체를 만들어서 비교하는 것은 크게 어렵진 않은데 그런 것없이 다음 순열을 찾는 규칙을 찾으려니 어렵다.
- * 머리를 식힌 후에 다시해봐야겠다.
+/**
+ * Runtime: 72 ms, faster than 100.00% of JavaScript online submissions for Next Permutation.
+ * Memory Usage: 40.4 MB, less than 45.20% of JavaScript online submissions for Next Permutation.
  */
 /**
  * 123
@@ -40,6 +43,7 @@ const nextPermutation = function(nums) {
  * 312
  * 321
  */
+nextPermutation([2, 3, 1]);
 nextPermutation([1, 2]);
 nextPermutation([1, 1]);
 nextPermutation([1, 3, 2]);
