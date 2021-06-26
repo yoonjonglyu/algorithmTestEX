5a47b4f2aa7ac8a7a70fdf7294e017957de5e92c
Isa(yunjongryu) <yunjonglyu@gmail.com>
2021-06-26 23:57:16 +0900

    feat: LEETCODE EXAMPLE 50 - 1~100/POW (X,N)

---
+++ leetcode/1~100/50. Pow(x, n).js
@@ -0,0 +1,12 @@
+/**
+ * @param {number} x
+ * @param {number} n
+ * @return {number}
+ */
+const myPow = function(x, n) {
+    return x**n;
+};
+/**
+ * Runtime: 80 ms, faster than 71.97 % of JavaScript online submissions for Pow(x, n).
+ * Memory Usage: 39.4 MB, less than 34.88 % of JavaScript online submissions for Pow(x, n).
+ */
