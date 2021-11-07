c17a802c76cb193a7c016edd5fa811b399acff08
Isa(yunjongryu) <yunjonglyu@gmail.com>
2021-11-07 19:35:29 +0900

    feat: PROGRAMERS LEVEL2 - 순위 검색 효율성 충족

--- programmers/level2/순위 검색.js
+++ programmers/level2/순위 검색.js
@@ -100,16 +100,73 @@ function solution(info, query) {
     const database = new Map();
 
     info.map((arr) => arr.split(' '))
-        .forEach((data, idx) => {
-            const [lang, position, career, food, recode] = data;
+        .forEach((data) => {
+            const recode = parseInt(data.pop());
 
-            const combination = () => {
+            const combination = (arr, recode, start) => {
+                const key = arr.join('');
+                if (!database.has(key)) database.set(key, []);
+                database.get(key).push(recode);
 
+                for (let int = start; int < arr.length; int++) {
+                    const next = Array.from(arr);
+                    next[int] = '-';
+                    combination(next, recode, int + 1);
+                }
             }
+
+            combination(data, recode, 0);
         });
 
-    return query.reduce((result, current) => {
+    for (const key of database.keys()) {
+        database.get(key).sort((a, b) => a - b);
+    }
 
+    return query.reduce((result, current) => {
+        const key = current.replace(/ and /g, ' ').split(' ');
+        const recode = parseInt(key.pop());
+        const data = database.get(key.join(''));
+        if (data) {
+            let left = 0;
+            let right = data.length;
+            while (left < right) {
+                const pivot = Math.floor((left + right) / 2);
+                if (data[pivot] >= recode) {
+                    right = pivot;
+                } else if (data[pivot] < recode) {
+                    left = pivot + 1;
+                }
+            }
+            result.push(data.length - left);
+        } else {
+            result.push(0);
+        }
         return result;
     }, []);
+}
+
+/** 다른 사람 풀이 수준차이를 느꼈다... 비트마스크 활용  */
+
+function bisect_gt(a, x) {
+    let lo = 0, hi = a.length;
+    while (lo < hi) {
+        let mid = Math.floor((lo + hi) / 2);
+        if (a[mid] < x) lo = mid + 1;
+        else hi = mid;
+    }
+    return a.length - lo;
+}
+
+function solution(info, query) {
+    const table = {"c": 3, "j": 5, "p": 6, "b": 6, "f": 5, "s": 6, "-": 0};
+    const conv = (l, t) => [l.slice(0, -1).reduce((a, k) => (a << 3) + t(table[k[0]]), 0), Number(l[l.length - 1])];
+    info = info.map(s => conv(s.split(" "), x => 7 - x));
+    query = query.map(s => conv(s.split(" ").filter(c => c != "and"), x => x));
+    const map = new Map();
+    for (const [k, v] of info) {
+        if (!map.has(k)) map.set(k, []);
+        map.get(k).push(v);
+    }
+    const dict = Array.from(map.entries()).map(([k, l]) => [k, l.sort((a, b) => a - b)])
+    return query.map(([q, v]) => dict.reduce((a, [k, l]) => a + (k & q ? 0 : bisect_gt(l, v)), 0));
 }
