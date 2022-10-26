08cd0fa8cc211a76e2204e66a55eefb40342ce1a
Isa(yunjongryu) <yunjonglyu@gmail.com>
2022-10-26 14:01:53 +0900

    feat(프로그래머스): 레벨0 - 두 수의 차

---
+++ programmers/level0/두 수의 차.js
@@ -0,0 +1,13 @@
+// 정수 num1과 num2가 주어질 때, num1에서 num2를 뺀 값을 return하도록 soltuion 함수를 완성해주세요.
+
+// 제한사항
+// -50000 ≤ num1 ≤ 50000
+// -50000 ≤ num2 ≤ 50000
+// 입출력 예
+// num1	num2	result
+// 2	3	-1
+// 100	2	98
+
+function solution(num1, num2) {
+  return num1 - num2;
+}
