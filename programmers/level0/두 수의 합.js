e9aaa6753a79f4e60a83020bfcfd7ae3efaeaa90
Isa(yunjongryu) <yunjonglyu@gmail.com>
2022-10-26 12:01:25 +0900

    feat(프로그래머스): 두 수의 합

---
+++ programmers/level0/두 수의 합.js
@@ -0,0 +1,13 @@
+// 정수 num1과 num2가 주어질 때, num1과 num2의 합을 return하도록 soltuion 함수를 완성해주세요.
+
+// 제한사항
+// -50,000 ≤ num1 ≤ 50,000
+// -50,000 ≤ num2 ≤ 50,000
+// 입출력 예
+// num1	num2	result
+// 2	3	5
+// 100	2	102
+// 입출력 예 설명
+function solution(num1, num2) {
+  return num1 + num2;
+}
